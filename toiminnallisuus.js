c68d55db2b6912621f26b271be68dba3d02eec10
LoserPanda <losarpanda82@gmail.com>
2018-06-20 11:31:10 +0300

    committaa kaikki

---
+++ toiminnallisuus.js
@@ -0,0 +1,15 @@
+function hae() {
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                //document.getElementById("demo").innerHTML =
+                //    this.responseText;
+                var teksti = this.responseText;
+                var obj = JSON.parse(teksti);
+                document.getElementById("demo").innerHTML =
+                    obj.value[0].joke;
+            }
+        };
+        req.open("GET", "http://api.icndb.com/jokes/random/1", true);
+        req.send();
+}
